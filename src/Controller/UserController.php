d6c70e4ec100f919790a80714a391bff5828f286
trishcaburian <p.caburian0311@gmail.com>
2020-08-27 18:38:18 +0800

    Made use of PasswordEncoder

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -8,14 +8,16 @@ use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\User;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class UserController extends AbstractController
 {
-    private $entityManager;
+    private $entityManager, $passEncoder;
 
-    public function __construct(EntityManagerInterface $entityManager)
+    public function __construct(EntityManagerInterface $entityManager, UserPasswordEncoderInterface $passEncoder)
     {
         $this->entityManager = $entityManager;
+        $this->passEncoder = $passEncoder;
     }
 
     /**
@@ -29,7 +31,7 @@ class UserController extends AbstractController
 
         $user = new User();
         $user->setUsername($username);
-        $user->setPassword($password);
+        $user->setPassword($this->passEncoder->encodePassword($user, $password));
 
         $this->entityManager->persist($user);
 
