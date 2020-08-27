69b395dd0a9c8986faa412c2501567d7fc04d053
trishcaburian <p.caburian0311@gmail.com>
2020-08-27 20:39:12 +0800

    added error message on failed login, again

--- src/Security/LoginAuthenticator.php
+++ src/Security/LoginAuthenticator.php
@@ -9,16 +9,18 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
-use Symfony\Component\Security\Core\Exception\CustomUserMessageAuthenticationException;
-use Symfony\Component\Security\Core\Exception\InvalidCsrfTokenException;
+use Symfony\Component\Security\Core\Exception\AuthenticationException;
+// use Symfony\Component\Security\Core\Exception\CustomUserMessageAuthenticationException;
+// use Symfony\Component\Security\Core\Exception\InvalidCsrfTokenException;
 use Symfony\Component\Security\Core\Security;
 use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Security\Core\User\UserProviderInterface;
-use Symfony\Component\Security\Csrf\CsrfToken;
+// use Symfony\Component\Security\Csrf\CsrfToken;
 use Symfony\Component\Security\Csrf\CsrfTokenManagerInterface;
 use Symfony\Component\Security\Guard\Authenticator\AbstractFormLoginAuthenticator;
 use Symfony\Component\Security\Guard\PasswordAuthenticatedInterface;
 use Symfony\Component\Security\Http\Util\TargetPathTrait;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 
 class LoginAuthenticator extends AbstractFormLoginAuthenticator implements PasswordAuthenticatedInterface
 {
@@ -62,16 +64,7 @@ class LoginAuthenticator extends AbstractFormLoginAuthenticator implements Passw
 
     public function getUser($credentials, UserProviderInterface $userProvider)
     {
-        // $token = new CsrfToken('authenticate', $credentials['csrf_token']);
-        // if (!$this->csrfTokenManager->isTokenValid($token)) {
-        //     throw new InvalidCsrfTokenException();
-        // }
-
         $user = $this->entityManager->getRepository(User::class)->findOneBy(['username' => $credentials['username']]);
-
-        if (!$user) {
-            echo "<script>alert('Invalid Username or Password');</script>";
-        }
 
         return $user;
     }
@@ -87,6 +80,13 @@ class LoginAuthenticator extends AbstractFormLoginAuthenticator implements Passw
     public function getPassword($credentials): ?string
     {
         return $credentials['password'];
+    }
+
+    public function onAuthenticationFailure(Request $request, AuthenticationException $exception)
+    {
+        echo "<script>alert('Invalid Username/Password. Please try again.');</script>";
+
+        header("Location: ".$this->urlGenerator->generate('app_login'));
     }
 
     public function onAuthenticationSuccess(Request $request, TokenInterface $token, string $providerKey)
