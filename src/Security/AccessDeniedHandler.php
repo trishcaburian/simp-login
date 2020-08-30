8eecf9485394036cf20850b930916389f6644a66
trishcaburian <p.caburian0311@gmail.com>
2020-08-30 21:28:09 +0800

    Modified AccessDeniedHandler to make use of errors/default.html.twig

--- src/Security/AccessDeniedHandler.php
+++ src/Security/AccessDeniedHandler.php
@@ -6,10 +6,20 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 use Symfony\Component\Security\Http\Authorization\AccessDeniedHandlerInterface;
 
+use Twig\Environment;
+
 class AccessDeniedHandler implements AccessDeniedHandlerInterface
 {
+    private $twigEnvironment;
+
+    public function __construct(Environment $twigEnvironment)
+    {
+        $this->twigEnvironment = $twigEnvironment;
+    }
+
     public function handle(Request $request, AccessDeniedException $accessDeniedException)
     {
-        return new Response("Sorry! This page is off limits! :(", 403);
+        $message = "Sorry, this page is off limits! :(";
+        return new Response($this->twigEnvironment->render('errors/default.html.twig', ['error_message' => $message]), 403);
     }
 }
