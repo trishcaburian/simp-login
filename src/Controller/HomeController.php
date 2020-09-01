48b00b30141750623fcd1a697581299b072d8ba6
trishcaburian <p.caburian0311@gmail.com>
2020-09-01 16:34:07 +0800

    can now generate forms via FormGenerator

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -1,10 +1,13 @@
 <?php
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Services\FormGenerator;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\Security;
+
 
 class HomeController extends AbstractController
 {
@@ -39,6 +42,10 @@ class HomeController extends AbstractController
     */
     public function loadRegisterPage()
     {
-        return $this->render("pages/register.html.twig");
+        $user = new User();
+
+        $form = $this->createForm(FormGenerator::class, $user);
+
+        return $this->render("pages/register.html.twig", ['form' => $form->createView()]);
     }
 }
