4b26957a4cd7825e49112fa565bc8641d5231451
trishcaburian <p.caburian0311@gmail.com>
2020-09-01 13:52:43 +0800

    removed the redundant js code for submitting to a route

--- public/assets/js/form_scripts.js
+++ public/assets/js/form_scripts.js
@@ -1,1 +1,22 @@
-console.log("ohai mark");
+function submitRequest(e, path){
+    console.log('test');
+    e.preventDefault();
+
+    var msg_list = document.querySelector('#msg_list');
+
+    if (msg_list !== null) {
+        msg_list.parentNode.removeChild(msg_list);
+    }
+
+    $.ajax({
+        url: path,
+        type: 'post',
+        data:$('#siteForm').serialize(),
+        success:function(result){
+            document.querySelector('#messages').insertAdjacentHTML('beforeend', result);
+        },
+        error: function(result){
+            console.log(result);
+        }
+    });
+}
